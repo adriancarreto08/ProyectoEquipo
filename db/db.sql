00df242fc745bc0a18e3a2c3a4a7604adadbb05c
Rafael Chaidez <rafael.chaidez.r@gmail.com>
2023-03-30 13:53:13 -0700

    Docker-composer

---
+++ db/db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE dht11 (
+    id int(11) unsigned NOT NULL AUTO_INCREMENT,
+    temperatura double DEFAULT NULL,
+    humedad double DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
