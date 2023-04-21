48f54e402487a3d8716c893f1bd840c91069018e
Rafael Chaidez <70284550+ingrafaelcr@users.noreply.github.com>
2023-04-20 17:46:44 -0700

    Actualizacion de bd

--- db/db.sql
+++ db/db.sql
@@ -2,5 +2,7 @@ CREATE TABLE dht11 (
     id int(11) unsigned NOT NULL AUTO_INCREMENT,
     temperatura double DEFAULT NULL,
     humedad double DEFAULT NULL,
+    indice double DEFAULT NULL,
+    fecha datetime DEFAULT current_timestamp(),
     PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
